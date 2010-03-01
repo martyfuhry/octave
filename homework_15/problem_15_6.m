c7fb99a21fb8d147baa4d3d94de5b5a77270e5fb
martyfuhry <martyfuhry@gmail.com>
2010-03-01 10:58:58 -0500

    homework 18

--- homework_15/problem_15_6.m
+++ homework_15/problem_15_6.m
@@ -5,7 +5,7 @@ M = 1.989e30; % mass of sun
 G = 6.67e-11; % gravitational constant
 
 % define a few constants
-global theta = G*M / (r*v^2); 
+theta = G*M / (r*v^2); 
 x     = 0;
 
 % function we want to zero
@@ -18,9 +18,7 @@ function fp = fp(z,theta)
     fp = 2*(1 + z) - 3 * theta * z^2;
 end
 
-count = 0;
 % use Newton's Method
 while abs(f(x,theta)) > 1e-16
    x = x -f(x,theta) / fp(x,theta); 
-    count = count+ 1;
 end
